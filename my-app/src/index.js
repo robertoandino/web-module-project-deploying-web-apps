b9c4888d5e2f8430be64ff801d62f1777014aa9f
Roberto Andino <andinorobert518@yahoo.com>
2024-09-05 20:02:55 -0400

    wip

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,3 +15,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+//"eslintConfig": {
+    //"extends": [
+      //"react-app",
+      //"react-app/jest"
+    //]
+  //},
